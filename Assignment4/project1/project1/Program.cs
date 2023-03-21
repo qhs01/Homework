bc398ddfcbc61b7f8a8cf5f0648bb0b12758faae
qhs01 <127219083+qhs01@users.noreply.github.com>
2023-03-21 18:50:20 +0800

    Add files via upload

---
+++ Assignment4/project1/project1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace project1 { 
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public T Sum
+        {
+            get; set;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action <Node<T>> action)
+        {
+            Node<T> t=head;
+            while (t != null)
+            {
+                action(t);
+                t = t.Next;
+            }
+        }
+    }
+    public class Test
+    {
+        public static void Main()
+        {
+            GenericList<int> Glist = new GenericList<int>();
+            int sum = 0;
+            int max = 0;
+            int min = 0x7fffffff;
+            Glist.Add(8);//创建链表
+            Glist.Add(3);
+            Glist.Add(-5);
+            Glist.Add(-2); 
+            Glist.Add(1);
+
+            Console.Write("链表的元素为：");//逐个输出链表元素
+            Glist.ForEach(n => Console.Write(n.Data+"->"));
+            Console.WriteLine();
+
+            Glist.ForEach(n => sum+=n.Data);//计算链表元素之和并输出
+            Console.Write("链表的元素的和为："+sum);
+            Console.WriteLine();
+
+            Glist.ForEach(n => max = max < n.Data ? n.Data : max);//计算链表元素的最大值并输出
+            Console.Write("链表元素的最大值为：" + max);
+            Console.WriteLine();
+
+            Glist.ForEach(n => min = min < n.Data ? min : n.Data);//计算链表元素的最小值并输出
+            Console.Write("链表元素的最小值为：" + min);
+
+        }
+    }
+}
---
+++ Assignment4/project2/project2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2
+{
+    class Clock
+    {
+
+        DateTime alarmTime = DateTime.Now;
+
+        public delegate void AlarmHandler(object sender, DateTime args);
+        public delegate void TickHanlder(object sender, DateTime args);
+
+        public event AlarmHandler OnAlarm;
+        public event TickHanlder OnTick;
+        public void PrintTime(object sender, DateTime time)
+        {
+            Console.Write(time);
+            Console.WriteLine();
+        }
+        public void Alarm(object sender, DateTime time)
+        {
+            Console.Write("闹钟响，现在的时间为：");
+        }
+
+        public void Tick(object sender, DateTime time)
+        {
+            Console.Write("嘀嗒，现在的时间为：");
+        }
+        public Clock()
+        {
+            OnAlarm += Alarm;
+            OnAlarm += PrintTime;
+            OnTick += Tick;
+            OnTick += PrintTime;
+        }
+        public void ClockStart()
+        {
+            Console.WriteLine("闹钟开始工作");
+            while (true)
+            {
+                DateTime nowtime = DateTime.Now;
+                OnTick(this, nowtime);
+                if (nowtime.ToString() == alarmTime.ToString()) 
+                {
+                    OnAlarm(this, alarmTime);
+                    break;
+                }
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+        public void SetalarmTime(DateTime alarmtime)
+        {
+            Console.WriteLine("所设的闹钟时间为："+alarmtime);
+            alarmTime = alarmtime;
+        }
+    }
+    class Project2
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            DateTime alarmtime = new DateTime();
+            alarmtime = DateTime.Now.AddSeconds(7);
+            clock.SetalarmTime(alarmtime);
+            clock.ClockStart();
+        }
+    }
+}
